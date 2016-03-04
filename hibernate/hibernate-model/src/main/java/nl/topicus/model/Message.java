acf17abde3b14aec9a9091bdd7fb36345c345e1d
Jordy Onrust <jordy.onrust@topicus.nl>
2016-03-04 12:38:22 +0100

    Init.

---
+++ hibernate/hibernate-model/src/main/java/nl/topicus/model/Message.java
@@ -0,0 +1,39 @@
+package nl.topicus.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message
+{
+  /*
+        Every persistent entity class must have an identifier
+        attribute annotated with <code>@Id</code>. Hibernate maps
+        this attribute to a column named <code>ID</code>.
+     */
+  @Id
+    /*
+        Someone must generate identifier values; this annotation enables
+        automatic generation of IDs.
+     */
+  @GeneratedValue
+  private Long id;
+
+  /*
+        You usually implement regular attributes of a persistent class with private
+        or protected fields, and public getter/setter method pairs. Hibernate maps
+        this attribute to a column called <code>TEXT</code>.
+     */
+  private String text;
+
+  public String getText() {
+	return text;
+  }
+
+  public void setText(String text) {
+	this.text = text;
+  }
+
+}
